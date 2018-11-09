036fdc66f0599ec48ac59167713c379a30776a1c
Mo Zhu <mozhu888@gmail.com>
2018-11-08 19:26:35 -0500

    show game results

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -7,6 +7,7 @@ class Game:
         self.moves = []
         self.player1 = Player(player1_brain, 1)
         self.player2 = Player(player2_brain, -1)
+        self.invalid_moves = 0
 
     def start(self):
         self.start_new_round(self.player1, self.player2)
@@ -15,6 +16,7 @@ class Game:
         move = mover.prompt(self.board)
 
         if not self.board.is_valid_move(move):
+            self.invalid_moves += 1
             mover.receive_invalid_move(self.board, move)
             self.start_new_round(mover, waiter)
             return
@@ -23,9 +25,11 @@ class Game:
         self.board.update(move)
 
         if self.board.has_winner():
+            self.winner = mover.token
             mover.receive_result(self.moves, 1)
             waiter.receive_result(self.moves, -1)
         elif self.board.is_tied():
+            self.winner = 0
             mover.receive_result(self.moves, 0)
             waiter.receive_result(self.moves, 0)
         else:
--- tic-tac-toe/runner.py
+++ tic-tac-toe/runner.py
@@ -4,8 +4,8 @@ from game import Game
 brain = AIBrain("dense")
 
 for i in range(100):
-    print "{num}\r".format(num=i)
-    ttt = Game(brain, brain)
-    ttt.start()
+    game = Game(brain, brain)
+    game.start()
+    print "{num}, {winner}, {invalid_moves}".format(num=i, winner=game.winner, invalid_moves=game.invalid_moves)
 
 brain.save()
