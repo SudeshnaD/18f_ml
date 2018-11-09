32fc18be6ed7ea51ab1212211c91c3900942ff82
Mo Zhu <mozhu888@gmail.com>
2018-11-09 09:50:01 -0500

    also save record of invalid games

--- tic-tac-toe/runner.py
+++ tic-tac-toe/runner.py
@@ -13,14 +13,14 @@ with open("{filename}.csv".format(filename=experiment_name), 'ab') as csvfile:
     csv_writer = csv.writer(csvfile)
 
     for i in range(N_RUNS):
-        # print "Game #{num}".format(num=i)
+        print "Game #{num}".format(num=i)
         game = Game(brain, brain)
         game.start()
         if game.winner:
-            print "{num}. {winner}, {invalid_moves}".format(num=i, winner=game.winner, invalid_moves=game.invalid_moves)
             csv_writer.writerow([game.winner, game.invalid_moves])
         else:
-            print "{num}, XXX, {invalid_moves}".format(num=i, invalid_moves=game.invalid_moves)
+            csv_writer.writerow(["XXX", game.invalid_moves])
 
     print "\n\n\n"
+
 brain.save()
